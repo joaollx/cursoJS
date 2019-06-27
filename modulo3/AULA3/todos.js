1ef1de839b7c9adf68bff2c796b34dfcec3be000
Joaollx <joao_pedro-silva@estudante.sc.senai.br>
2019-06-27 18:29:25 -0300

    aula 3 finalizada inicio aula 4

---
+++ modulo3/AULA3/todos.js
@@ -0,0 +1,25 @@
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+
+var buttonElement = document.querySelector("#app button")
+
+var todos = [
+    'fazer café',
+    'beber água',
+    'reciclar'
+
+];
+
+function renderTodos() {
+    for (todo of todos) {
+
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+    }
+
+}
+
+renderTodos();
